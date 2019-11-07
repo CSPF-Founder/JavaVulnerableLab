368b09c04070913f1297772e5c1b14375474dee1
skoussa <sherif@softwaresecured.com>
2019-11-07 16:25:00 -0500

    hello

--- src/main/java/org/cysecurity/cspf/jvl/controller/LoginValidator.java
+++ src/main/java/org/cysecurity/cspf/jvl/controller/LoginValidator.java
@@ -49,7 +49,7 @@ public class LoginValidator extends HttpServlet {
                                {
                                    ResultSet rs=null;
                                    Statement stmt = con.createStatement();  
-                                   rs=stmt.executeQuery("select * from users where username='"+user+"' and password='"+pass+"'");
+                                   rs=stmt.executeQuery("select * from users where username=? and password=?");
                                    if(rs != null && rs.next()){
                                    HttpSession session=request.getSession();
                                    session.setAttribute("isLoggedIn", "1");
