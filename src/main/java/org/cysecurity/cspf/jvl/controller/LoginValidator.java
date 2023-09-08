4324d65b1dc04d9ebc7dee2912e3b82506be1b98
statera <tech@staterasolv.com>
2023-09-08 14:04:08 +0800

    Added back the SQLI vulnerability

--- src/main/java/org/cysecurity/cspf/jvl/controller/LoginValidator.java
+++ src/main/java/org/cysecurity/cspf/jvl/controller/LoginValidator.java
@@ -9,7 +9,6 @@ package org.cysecurity.cspf.jvl.controller;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import javax.servlet.ServletException;
@@ -43,29 +42,26 @@ public class LoginValidator extends HttpServlet {
 
         String user=request.getParameter("username").trim();
         String pass=request.getParameter("password").trim();
-
         try
         {
             Connection con=new DBConnect().connect(getServletContext().getRealPath("/WEB-INF/config.properties"));
             if(con!=null && !con.isClosed())
             {
                 ResultSet rs=null;
-                PreparedStatement pstmt = con.prepareStatement("select * from users where username=? and password=?");
-                pstmt.setString(1, user);
-                pstmt.setString(2, pass);
-                rs = pstmt.executeQuery();
-                if(rs != null && rs.next()) {
-                    HttpSession session = request.getSession();
+                Statement stmt = con.createStatement();
+                rs=stmt.executeQuery("select * from users where username='"+user+"' and password='"+pass+"'");
+                if(rs != null && rs.next()){
+                    HttpSession session=request.getSession();
                     session.setAttribute("isLoggedIn", "1");
                     session.setAttribute("userid", rs.getString("id"));
                     session.setAttribute("user", rs.getString("username"));
                     session.setAttribute("avatar", rs.getString("avatar"));
-                    Cookie privilege = new Cookie("privilege", "user");
+                    Cookie privilege=new Cookie("privilege","user");
                     response.addCookie(privilege);
-
-                    if (request.getParameter("RememberMe") != null) {
-                        Cookie username = new Cookie("username", user);
-                        Cookie password = new Cookie("password", pass);
+                    if(request.getParameter("RememberMe")!=null)
+                    {
+                        Cookie username=new Cookie("username",user);
+                        Cookie password=new Cookie("password",pass);
                         response.addCookie(username);
                         response.addCookie(password);
                     }
@@ -75,10 +71,9 @@ public class LoginValidator extends HttpServlet {
                 {
                     response.sendRedirect("ForwardMe?location=/login.jsp&err=Invalid Username or Password");
                 }
+
             }
-
         }
-
         catch(Exception ex)
         {
             response.sendRedirect("login.jsp?err=something went wrong");
