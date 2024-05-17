1b6947ad9d6e82887614b726fe486dfc23f2a484
shaoyangchin <chinshaoyang343@gmail.com>
2024-05-17 19:45:33 +0800

    added new hardcoded code

--- src/main/java/org/cysecurity/cspf/jvl/controller/users.java
+++ src/main/java/org/cysecurity/cspf/jvl/controller/users.java
@@ -2,10 +2,10 @@ package org.cysecurity.cspf.jvl.controller;
 
 public class users {
     int VerifyAdmin(String password) {
-        if (!password.equals("Mew!")) {
+        if (!password.equals("Meow!")) {
         return(0);
         }
-        //Diagnostic Mode
+
         return(1);
         }
 }
